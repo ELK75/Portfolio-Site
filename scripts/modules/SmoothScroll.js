55f6519f27c4c4444e1a28598430f8c2e847da7a
ELK75 <elijah.kajinic@gmail.com>
2018-08-07 17:14:24 -0400

    fixed navbar bug

--- scripts/modules/SmoothScroll.js
+++ scripts/modules/SmoothScroll.js
@@ -1,12 +1,12 @@
 import $ from 'jquery';
 
 module.export = function() {
-    $(".nav__item").click(function(e) {
+    $(".nav__item__link").click(function(e) {
         var sectionID = e.currentTarget.id + "Section";
 
         $("html body").animate({
             scrollTop: $("#" + sectionID).offset().top - 60
-        }, 1000);
+        }, 500);
 
         $('.nav__hamburger-menu').removeClass('animate');
         $('.nav__items').removeClass('nav__height-auto');
